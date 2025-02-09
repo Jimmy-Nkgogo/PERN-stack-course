a12ed1beebd1ff6fe5a8a5d6b6da55f8240a7f6d
Jimmy <nkgogojimmy@gmail.com>
2025-02-09 02:36:02 +0200

    created the get and post controllers

--- backend/server.js
+++ backend/server.js
@@ -3,21 +3,20 @@ import helmet from "helmet";
 import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
 const app = express();
+
 const PORT = process.env.PORT || 3000;
+
 app.use(express.json());
 app.use(cors("http"));
-
 app.use(helmet()); // this helps with security by setting various HTTP headers
 app.use(morgan("dev")); // log the requests
 
-app.get("/test", (req, res) => {
-  console.log(res.getHeaders());
-  res.send("Hello");
-});
+app.use("/api/products", productRoutes)
 
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
