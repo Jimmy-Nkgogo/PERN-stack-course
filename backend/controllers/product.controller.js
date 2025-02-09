a83c4e93f6c1f3beea861866c392c8ac0aaf5b69
Jimmy <nkgogojimmy@gmail.com>
2025-02-09 03:34:51 +0200

    createProduct endpoint works

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,28 +1,43 @@
-import { sql } from "../config/db.js"
+import { sql } from "../config/db.js";
 
 export const getAllProducts = async (req, res) => {
-    try {
-       const products = await sql`SELECT * FROM products ORDER BY created_at DESC`
+  try {
+    const products = await sql`SELECT * FROM products ORDER BY created_at DESC`;
 
-       res.status(200).json({success: true, data: products});
-       console.log("Fetched products", products)
-    } catch (error) {
-        
-    }
-    
-}
-export const getProduct = async (req, res) => {
-    // GET ALL PRODUCTS
-    
-}
+    res.status(200).json({ success: true, data: products });
+    console.log("Fetched products", products);
+  } catch (error) {
+    console.log("Error in getAllProducts", error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Something went wrong." });
+  }
+};
+export const getProduct = async (req, res) => {};
+
 export const createProduct = async (req, res) => {
-    // CREATE A PRODUCT
-}
+  const { name, price, image } = req.body;
+  if (!name || !price || !image) {
+    return res
+      .status(400)
+      .json({ success: false, message: "All fields are required" });
+  }
+  try {
+    const newProduct = await sql`
+          INSERT INTO products (name,price,image) VALUES (${name},${price},${image})
+          RETURNING *
+          `;
 
-export const updateProduct = async (req, res ) => {
+    console.log("new product added: ", newProduct);
+    return res.status(201).json({ success: true, data: newProduct[0] });
+  } catch (error) {
+    console.log("Error in createProduct", error);
+    return res
+      .status(400)
+      .json({ success: false, message: "Something went wrong" });
+  }
+};
 
-}
+export const updateProduct = async (req, res) => {};
 
-export const deleteProduct = async (req, res ) => {
-
-}
+export const deleteProduct = async (req, res) => {};
