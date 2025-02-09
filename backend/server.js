6c0627ab64b2d44058569535e52a7ca60d16c9cb
Jimmy <nkgogojimmy@gmail.com>
2025-02-09 02:27:19 +0200

    done with basic set up of server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,24 @@
-import express from "express"
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.json());
+app.use(cors("http"));
+
+app.use(helmet()); // this helps with security by setting various HTTP headers
+app.use(morgan("dev")); // log the requests
+
+app.get("/test", (req, res) => {
+  console.log(res.getHeaders());
+  res.send("Hello");
+});
+
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
+});
