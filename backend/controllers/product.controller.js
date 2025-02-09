7724e185c3352e42c3b37e59ebf8a33b0a69f392
Jimmy <nkgogojimmy@gmail.com>
2025-02-09 03:08:40 +0200

    done with db setup

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,5 +1,8 @@
+import { sql } from "../config/db.js"
+
 export const getAllProducts = async (req, res) => {
     // GET ALL PRODUCTS
+    
 }
 
 export const createProduct = async (req, res) => {
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -4,7 +4,7 @@ import { createProduct, getAllProducts } from "../controllers/product.controller
 const router = express.Router();
 
 router.get("/", getAllProducts);
-
 router.post("/", createProduct)
+
 
 export default router;
