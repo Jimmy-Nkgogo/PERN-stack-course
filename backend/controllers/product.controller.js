731636b49d4bbb84c34983967355f492dc363974
Jimmy <nkgogojimmy@gmail.com>
2025-02-09 14:20:54 +0200

    getProduct works

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -13,28 +13,40 @@ export const getAllProducts = async (req, res) => {
       .json({ success: false, message: "Something went wrong." });
   }
 };
-export const getProduct = async (req, res) => {};
-
 export const createProduct = async (req, res) => {
   const { name, price, image } = req.body;
   if (!name || !price || !image) {
     return res
-      .status(400)
-      .json({ success: false, message: "All fields are required" });
+    .status(400)
+    .json({ success: false, message: "All fields are required" });
   }
   try {
     const newProduct = await sql`
-          INSERT INTO products (name,price,image) VALUES (${name},${price},${image})
-          RETURNING *
-          `;
-
+    INSERT INTO products (name,price,image) VALUES (${name},${price},${image})
+    RETURNING *
+    `;
+    
     console.log("new product added: ", newProduct);
     return res.status(201).json({ success: true, data: newProduct[0] });
   } catch (error) {
     console.log("Error in createProduct", error);
     return res
-      .status(400)
-      .json({ success: false, message: "Something went wrong" });
+    .status(400)
+    .json({ success: false, message: "Something went wrong" });
+  }
+};
+export const getProduct = async (req, res) => {
+  const {id} = req.params;
+
+  try {
+    const product = await sql`
+    SELECT * FROM products WHERE id = ${id}
+    
+    `
+    return res.status(200).json({ success: true, data: product[0]})
+  } catch (error) {
+    console.log("Error in getProduct function", error);
+    res.status(500).json({success: false, message: "Internal Server Error"})
   }
 };
 
