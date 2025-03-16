41e82dd5f70117ba4da56452d0efd89c3a1d45ce
Jimmy <nkgogojimmy@gmail.com>
2025-03-16 05:01:29 +0200

    added the refresh button

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useProductStore } from "../store/useProductStore";
+import { PlusCircleIcon, RefreshCwIcon } from 'lucide-react'
 
 const HomePage = () => {
-  return (
-    <div>HomePage</div>
-  )
-}
+  const { products, fetchProducts, error, loading } = useProductStore();
 
-export default HomePage
+  useEffect(() => {
+    fetchProducts();
+  }, [fetchProducts]);
+
+  console.log(products);
+
+  return <main className="max-w-6xl mx-auto px-4 py-8">
+    <div className="flex justify-between items-center mb-8">
+      <button className="btn btn-primary">
+        <PlusCircleIcon className='size-5 mr-2' />
+        Add Product
+      </button>
+      <button className="btn btn-ghost btn-circle" onClick={fetchProducts}>
+        <RefreshCwIcon className="size-5" />
+      </button>
+    </div>
+  </main>;
+};
+
+export default HomePage;
